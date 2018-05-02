305a03943ae7d7c49524a40b5ab8c7e4be6fc9a1
RandaKhairy <randakhairy_95@hotmail.com>
2018-05-03 01:58:18 +0200

    Add files via upload
    takes bounding box coordinates of detected traffic light and use it to crop the traffic light from original image then detect its color

---
+++ process.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+
+img = cv2.imread('predictions.jpg',1)
+f=open("prediction.txt", "r")
+if f.mode != 'r':
+	print("Error Readin File")
+img = cv2.medianBlur(img,5)
+
+lines = f.readlines()
+for l in lines:
+	[x, y, w, h] = l.split()
+	x = int(float(x))
+	y = int(float(y))
+	w = int(float(w))
+	h = int(float(h))
+	starty = int(y - h/2)
+	startx = int(x - w/2)
+	crop_img = img[starty:starty+h, startx:startx+w]
+ 
+	red = np.mean(crop_img[:, :, 2])
+	green = np.mean(crop_img[:, :, 1])
+	print("Red mean = ", red)
+	print("Green mean = ", green)
+	if(red > green):
+		print("Red")
+	else:
+		print("Green")
+
+	
+	cv2.imshow('image',crop_img)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
